0f9bdcc573c4012352c06c876a9dfd71e2359ffb
Casper <TheGostKasper@gmail.com>
2018-06-20 15:08:08 +0200

    Create VngDepartment.cs

---
+++ EFContext/Models/VngDepartment.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFContext.Models
+{
+    public class VngDepartment
+    {
+        public int Id { get; set; }
+        public string NameEn { get; set; }
+        public string NameAr { get; set; }
+    }
+}
